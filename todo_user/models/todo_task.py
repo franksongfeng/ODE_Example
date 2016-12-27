d33e75fb22c468deebd15a3d62025ab773155aa9
franksongfeng <frank.song@elico-corp.com>
2016-12-27 17:44:29 +0800

    extend mixin, and social network

--- todo_user/models/todo_task.py
+++ todo_user/models/todo_task.py
@@ -3,42 +3,41 @@ from openerp import models, fields, api
 
 
 class TodoTask(models.Model):
-	_inherit = 'todo.task'
-	
-	user_id =fields.Many2one(
-		comodel_name='res.users',
-		string='Responsible'
-	)
-	
-	date_deadline=fields.Date(
-		string='Deadline'
-	)
+    _name = 'todo.task'
+    _inherit = ['todo.task', 'mail.thread']
 
-	name = fields.Char(
-		help="What needs to be done?"
-	)
+    user_id =fields.Many2one(
+        comodel_name='res.users',
+        string='Responsible'
+    )
+    date_deadline=fields.Date(
+        string='Deadline'
+    )
+    name = fields.Char(
+        help="What needs to be done?"
+    )
 
-	@api.multi
-	def do_clear_done(self):
-		import pdb
-		pdb.set_trace()
-		domain = [
-			'&',
-			('is_done', '=', True),
-			'|', 
-			('user_id', '=', self.env.uid),
-			('user_id', '=', False)
-		]
-		done_recs = self.search(domain)
-		if done_recs:
-			done_recs.write({'active': False})
-			return True
-		else:
-			return False
+    @api.multi
+    def do_clear_done(self):
+        import pdb
+        pdb.set_trace()
+        domain = [
+            '&',
+            ('is_done', '=', True),
+            '|',
+            ('user_id', '=', self.env.uid),
+            ('user_id', '=', False)
+        ]
+        done_recs = self.search(domain)
+        if done_recs:
+            done_recs.write({'active': False})
+            return True
+        else:
+            return False
 
-	@api.one
-	def do_toggle_done(self):
-		if self.user_id != self.env.user:
-			raise Exception('Only the responsible can do this!')
-		else:
-			return super(TodoTask, self).do_toggle_done()				
+    @api.one
+    def do_toggle_done(self):
+        if self.user_id != self.env.user:
+            raise Exception('Only the responsible can do this!')
+        else:
+            return super(TodoTask, self).do_toggle_done()
