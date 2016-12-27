2384f3cb99e3893e63fccb46be59254315ff50ae
franksongfeng <frank.song@elico-corp.com>
2016-12-27 16:52:38 +0800

    add module todo_user

---
+++ todo_user/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#   2016 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Multiuer To-Do',
+    'description': 'Extend the To-Do app to multiusesr.',
+    'category': 'Test',
+    'summary': 'Example in the book ODE',
+    'version': '9.0.1.0.0',
+    'website': 'www.elico-corp.com',
+    'author': 'Frank Song',
+    'depends': [
+        'todo_app',
+    ],
+    'data': [
+        'views/todo_view.xml',
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
+
