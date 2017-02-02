40a133735f675c31716126057e880e3b6978526a
franksongfeng <frank.song@elico-corp.com>
2017-02-02 22:31:31 +0800

    add todo_access_rules.xml for todo_user. implementioned Ch3 all.

--- todo_user/__openerp__.py
+++ todo_user/__openerp__.py
@@ -6,7 +6,7 @@
     'description': 'Extend the To-Do app to multiusesr.',
     'category': 'Test',
     'summary': 'Example in the book ODE',
-    'version': '9.0.1.0.2',
+    'version': '9.0.1.0.3',
     'website': 'www.elico-corp.com',
     'author': 'Frank Song',
     'depends': [
@@ -14,6 +14,7 @@
     ],
     'data': [
         'views/todo_view.xml',
+        'security/todo_access_rules.xml',
     ],
     'application': True,
     'installable': True,
