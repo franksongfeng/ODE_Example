7867365f6cf1bf7723f79a76d6055dc131bad365
franksongfeng <frank.song@elico-corp.com>
2017-02-20 17:42:58 +0800

    add todo_wizard

---
+++ todo_wizard/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'To-do Tasks Management Assistant',
+    'description': 'Mass edit your To-Do backlog.',
+    'category': 'Test',
+    'summary': 'Example in the book ODE',
+    'version': '9.0.2.0.1',
+    'website': 'www.elico-corp.com',
+    'author': 'Frank Song',
+    'depends': [
+        'todo_user',
+    ],
+    'data': ['todo_wizard_view.xml'],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
