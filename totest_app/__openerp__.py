44a18fa15d4f635519968b0d41e2ba150af4f453
franksongfeng <frank.song@elico-corp.com>
2017-03-21 10:14:22 +0800

    add totest_app, a singel test app

---
+++ totest_app/__openerp__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'To-Test Application',
+    'category': 'Test',
+    'summary': 'Example in the book ODE',
+    'version': '9.0.0.0.1',
+    'website': 'www.elico-corp.com',
+    'author': 'Frank Song',
+    'depends': [
+        'mail',
+    ],
+    'data': [
+        'views/view.xml',
+        'security/access_rules.xml',
+    ],
+    'images': [],
+    'license': 'AGPL-3',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
---
+++ totest_app/models/model.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+
+
+class TotestTask(models.Model):
+    _name = 'totest.task'
+    _description = 'To-Test-Task'
+    name = fields.Char(string='Description', required=True, size=64)
+    is_done = fields.Boolean(string='Done?')
+    active = fields.Boolean(string='Active?', default=True)
+
+    @api.one
+    def do_toggle_done(self):
+        self.is_done = not self.is_done
+        return True
+
+    @api.multi
+    def do_clear_done(self):
+        done_recs = self.search([('is_done', '=', True)])
+        done_recs.write({'active': False})
+        return True
